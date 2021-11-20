6bdbc396639daa7358403977b412d4043be0249e
unknown <vrnakano@gmail.com>
2021-11-20 20:50:01 -0300

    insert one more condition in verify-string file

--- verify-string.py
+++ verify-string.py
@@ -1,9 +1,11 @@
 # Lógica para verificar presença de números em uma string
 
-str1 = "python3"
+str1 = "python"
 if any(map(str.isdigit, str1)) == True:
     print("Possui número na string em alguma posição.")
     #raise ValueError("Possui número na string em alguma posição.") - estoura uma exceção.
+else:
+    print("Não tem número em nnehuma posição.")
 
 str2 = "11python"
 if len(str2) >= 2 and str2[0].isdigit() and str2[1].isdigit():
