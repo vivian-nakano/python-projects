38bb6e9162a187c640322af6a6034c05742cdf33
unknown <vrnakano@gmail.com>
2021-11-20 20:31:34 -0300

    insert one more condition in verify-string file

--- verify-string.py
+++ verify-string.py
@@ -5,6 +5,11 @@ if any(map(str.isdigit, str1)) == True:
     print("Possui número na string em alguma posição.")
     #raise ValueError("Possui número na string em alguma posição.") - estoura uma exceção.
 
-str2 = "12python"
+str2 = "11python"
 if len(str2) >= 2 and str2[0].isdigit() and str2[1].isdigit():
-    print('Possui número em alguma das duas primeiras casas.')
+    print('Possui número nos dois primeiros caracteres.')
+
+#verifica se a primeiro ou segundo caracter é número
+str3 = "texto"
+if str3[0].isdigit() or str3[1].isdigit():
+  print("Possui número em alguma das duas primeiras casas.")
