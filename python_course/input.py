0ec00a46eb8ee2f0e7d23e2fb1139c8b51cbef0f
unknown <vrnakano@gmail.com>
2022-02-19 19:07:09 -0300

    insert python dir - udemy course

---
+++ python_course/input.py
@@ -0,0 +1,12 @@
+# print("Function that simply print something, some text, some string, variable...")
+
+# input("Function that print some text and expect something.")
+
+name = input("What's your name?")
+
+print("A quantidade de caracteres são: ")
+(print(len(name)))
+
+
+
+
