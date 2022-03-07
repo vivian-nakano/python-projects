aefb8e8ede559c6ed9e2474a0947dcade2a1c911
unknown <vrnakano@gmail.com>
2022-03-07 19:51:51 -0300

    insert math operations

--- python_course/3day.py
+++ python_course/3day.py
@@ -5,3 +5,21 @@ print(int(6/3))
 
 print(2**100) #exponente
 
+#PEMDAS:
+# ()
+# **
+# * /
+# + -
+print(3 * 3 + 3 / 3 - 3)
+print(3 * (3 + 3) / - 3)
+
+#Exercise: BMI
+#BMI = weight(kg)/height^2
+
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+bmi = int(weight) / (float(height)*2)
+print(bmi)
+#bmi_as_int = int(bmi)
+
