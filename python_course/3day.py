be4f9d9dcab5dcd0d2b51d13cc5ffaa2532dbaf5
unknown <vrnakano@gmail.com>
2022-03-07 19:56:33 -0300

    tratativa de numeros

--- python_course/3day.py
+++ python_course/3day.py
@@ -15,11 +15,22 @@ print(3 * (3 + 3) / - 3)
 
 #Exercise: BMI
 #BMI = weight(kg)/height^2
+def calculate_bmi():
 
-height = input("enter your height in m: ")
-weight = input("enter your weight in kg: ")
+    height = input("enter your height in m: ")
+    weight = input("enter your weight in kg: ")
+    bmi = int(weight) / (float(height)*2)
+    print(bmi)
+    #bmi_as_int = int(bmi)
 
-bmi = int(weight) / (float(height)*2)
-print(bmi)
-#bmi_as_int = int(bmi)
+    #calculate_bmi()
+
+#Tratativa de números
+
+##Round numbers
+print(8/3)
+print(round(8/3, 2))
+
+##
+
 
