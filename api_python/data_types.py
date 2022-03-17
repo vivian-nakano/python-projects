826e5c6a30b08b27568a0d6a4a63f11676ef112c
unknown <vrnakano@gmail.com>
2022-03-17 20:32:07 -0300

    insert comments

--- api_python/data_types.py
+++ api_python/data_types.py
@@ -7,7 +7,7 @@ d['mynewkey'] = 'mynewvalue'
 
 print(d)
 
-
+##Organiza os campos de forma crescente
 orders = {
 	'cappuccino': 54,
 	'latte': 56,
@@ -17,6 +17,21 @@ orders = {
 }
 
 sort_orders = sorted(orders.items(), key=lambda x: x[1])
+
+for i in sort_orders:
+	print(i[0], i[1])
+
+
+##Organiza os campos de forma descrescente
+orders = {
+	'cappuccino': 54,
+	'latte': 56,
+	'espresso': 72,
+	'americano': 48,
+	'cortado': 41
+}
+
+sort_orders = sorted(orders.items(), key=lambda x: x[1], reverse=True)
 
 for i in sort_orders:
 	print(i[0], i[1])
