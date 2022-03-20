74202ba8531bd6d649518ab7a2276426cf816899
unknown <vrnakano@gmail.com>
2022-03-20 14:53:15 -0300

    organize functions

--- api_python/data_types.py
+++ api_python/data_types.py
@@ -1,37 +1,45 @@
 from collections import OrderedDict
 
-d = OrderedDict()
-d["server"] = "mpilgrim"
-d["database"] = "master"
-d['mynewkey'] = 'mynewvalue'
+def orderdict():
 
-print(d)
+	d = OrderedDict()
+	d["server"] = "mpilgrim"
+	d["database"] = "master"
+	d['mynewkey'] = 'mynewvalue'
 
-##Organiza os campos de forma crescente
-orders = {
-	'cappuccino': 54,
-	'latte': 56,
-	'espresso': 72,
-	'americano': 48,
-	'cortado': 41
-}
+	print(d)
 
-sort_orders = sorted(orders.items(), key=lambda x: x[1])
+orderdict()
 
-for i in sort_orders:
-	print(i[0], i[1])
+def organizar_crescente_desc():
+
+	##Organiza os campos de forma crescente
+	orders = {
+		'cappuccino': 54,
+		'latte': 56,
+		'espresso': 72,
+		'americano': 48,
+		'cortado': 41
+	}
+
+	sort_orders = sorted(orders.items(), key=lambda x: x[1])
+
+	for i in sort_orders:
+		print(i[0], i[1])
 
 
-##Organiza os campos de forma descrescente
-orders = {
-	'cappuccino': 54,
-	'latte': 56,
-	'espresso': 72,
-	'americano': 48,
-	'cortado': 41
-}
+	##Organiza os campos de forma descrescente
+	orders = {
+		'cappuccino': 54,
+		'latte': 56,
+		'espresso': 72,
+		'americano': 48,
+		'cortado': 41
+	}
 
-sort_orders = sorted(orders.items(), key=lambda x: x[1], reverse=True)
+	sort_orders = sorted(orders.items(), key=lambda x: x[1], reverse=True)
 
-for i in sort_orders:
-	print(i[0], i[1])
+	for i in sort_orders:
+		print(i[0], i[1])
+
+
