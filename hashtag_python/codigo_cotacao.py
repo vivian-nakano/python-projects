dfe8ec99ecf04177ee949f84257f6aab92177df1
vivian-nakano <vrnakano@gmail.com>
2022-05-10 15:25:14 -0300

    insert code python to get request

--- hashtag_python/codigo_cotacao.py
+++ hashtag_python/codigo_cotacao.py
@@ -1,4 +1,8 @@
 import requests
 
 def get_cotation():
-    request = requests.get()
+    request = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
+
+    request_dic = request.json()
+
+    dolar_cotation =
